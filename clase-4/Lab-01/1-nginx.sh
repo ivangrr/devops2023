716680f4458a86f4cf2ef6a998f697263894c447
Ivan Romero <ivangrr@gmail.com>
2023-05-09 18:44:51 -0400

    Lab-01 de la clase 4

---
+++ clase-4/Lab-01/1-nginx.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+docker pull nginx:1.22.1
+docker run --name=db -p 3306:3306 -e MYSQL_ROOT_PASSWORD=mysql_pwd -d mysql:8
+docker logs db
+docker run --name=my-admin -p 82:80 --link db:db -d phpmyadmin
+
+
